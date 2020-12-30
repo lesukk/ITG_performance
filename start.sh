f623df4d4c1e0d9e4c8cfca094fba5ff4eb01d7b
lesukk <lesukk2@gmail.com>
2020-12-30 15:22:11 +0200

    Update start.sh
    Changing repo for load tests

--- start.sh
+++ start.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 sudo rm  /home/start.sh
-sudo cp /Performance/start.sh /home/
+sudo cp /ITG_performance/start.sh /home/
 sudo chmod 744  /home/start.sh
 cd /
-#rm -rf /Performance
+rm -rf /ITG_performance
 wget https://bitbucket.org/pjtr/jmeter-websocket-samplers/downloads/JMeterWebSocketSamplers-1.2.8.jar
 mv JMeterWebSocketSamplers-1.2.8.jar /mnt/jmeter/apache-jmeter-5.3/lib
 
-echo " input  git clone ? >>>   1=   git  https://github.com/justUniverse13/Performance    2=  git clone -b websockets https://github.com/justUniverse13/Performance"
+echo " input  git clone ? >>>   1=  Polls    2=  Websockets"
 read input
 if [ "$input" = "1" ] ; then
-        echo " 1 =  git  https://github.com/justUniverse13/Performance"
-       sudo git clone https://github.com/justUniverse13/Performance
+        echo " 1 =  Poll test has been started"
+        sudo git clone https://github.com/lesukk/ITG_performance
+        sleep 10
+       sudo /mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /ITG_performance/scenario.jmx  -l /mnt/scenario.jtl
 else
         echo " 2 =  git clone -b websockets https://github.com/justUniverse13/Performance"
-        sudo git clone -b websockets https://github.com/justUniverse13/Performance
+        sudo git clone git clone https://github.com/lesukk/ITG_performance
+        sleep 10
+        sudo /mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /ITG_performance/ITG_WebSockets.jmx  -l /mnt/scenario.jtl
+        
 fi
 #git clone -b websockets https://github.com/justUniverse13/Performance
-sleep 15 
-/mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
+#sleep 15 
+#/mnt/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Performance/scenario.jmx  -l /mnt/scenario.jtl
