bff12f4efc9bfdf72c6caf32fb898cfaff141883
SydorakM <81176992+SydorakM@users.noreply.github.com>
2021-06-03 11:03:17 +0300

    Update start.sh
    Added login by token stress tests

--- start.sh
+++ start.sh
@@ -7,18 +7,25 @@ rm -rf /ITG_performance
 wget https://bitbucket.org/pjtr/jmeter-websocket-samplers/downloads/JMeterWebSocketSamplers-1.2.8.jar
 mv JMeterWebSocketSamplers-1.2.8.jar /mnt/jmeter/apache-jmeter-5.4.1/lib
 
-echo " input  git clone ? >>>   1=  Polls    2=  Websockets"
+echo " input  git clone ? >>>   1=  Polls    2=  Websockets"   3=  LoginByToken
 read input
 if [ "$input" = "1" ] ; then
         echo " 1 =  Poll test has been started"
         sudo git clone https://github.com/lesukk/ITG_performance
         sleep 10
        sudo /mnt/jmeter/apache-jmeter-5.4.1/bin/jmeter.sh -n -t /ITG_performance/scenario.jmx  -l /mnt/scenario.jtl
+
+elif     echo " 3 =  LoginByToken test has been started"
+        sudo git clone https://github.com/lesukk/ITG_performance
+        sleep 10
+        sudo /mnt/jmeter/apache-jmeter-5.4.1/bin/jmeter.sh -n -t /ITG_performance/login_by_token.jmx  -l /mnt/scenario.jtl
+
 else
         echo " 2 =  WebSocket test has been started"
         sudo git clone https://github.com/lesukk/ITG_performance
         sleep 10
         sudo /mnt/jmeter/apache-jmeter-5.4.1/bin/jmeter.sh -n -t /ITG_performance/ITG_WebSockets.jmx  -l /mnt/scenario.jtl
+
         
 fi
 #git clone -b websockets https://github.com/justUniverse13/Performance
